e98f4aa284517bcd57b79616f2cc2efdf07ec8a8
jjiahuang <15602215864@163.com>
2021-11-12 18:13:05 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const storiesReducer = (state, action) => {
+  if (action.type === 'SET_STORIES') {
+    return action.payload;
+  } else {
+    throw new Error();
+  }
+};
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+// 函数组件
+const App = () => {
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    []
+  );
+};
 
-function App() {
+const List = props =>
+  props.list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+    </div>
+  ));
+
+const storiesReducer = (state, action) => {
+  if (action.type === 'SET_STORIES') {
+    return action.payload;
+  } else if (action.type === 'REMOVE_STORY') {
+    return state.filter(
+      story => action.payload.objectID !== story.objectID
+    );
+  } else {
+    throw new Error();
+  }
+};
+const App = () => {
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    []
+  );
+  const [searchTerm, setSearchTerm] = React.useState('React');
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+  const searchedStories = stories.filter(function (story) {
+    return story.title.includes(searchTerm);
+  });
+  React.useEffect(() => {
+    setIsLoading(true);
+    getAsyncStories()
+      .then(result => {
+        dispatchStories({
+          type: 'SET_STORIES',
+          payload: result.data.stories,
+        });
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
+  }, []);
+  const handleRemoveStory = item => {
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search search={searchTerm} onSearch={handleSearch} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
-}
+};
 
+const Search = ({ search, onSearch }) => (
+  <div>
+    <input
+      id="search"
+      type="text"
+      value={search}
+      onChange={onSearch}
+    />
+  </div>
+);
 export default App;

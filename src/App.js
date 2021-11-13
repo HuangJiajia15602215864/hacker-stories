e39a4683482c81ea5622978bab3441da007c7258
jjiahuang <15602215864@163.com>
2021-11-13 16:22:52 +0800

    使用 async / await，使用form

--- src/App.js
+++ src/App.js
@@ -48,31 +48,30 @@ function App() {
     localStorage.setItem("search", searchTerm);
   }, [searchTerm]);
 
-  const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return;
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    axios
-      .get(url)
-      .then(result => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
-      .catch(() =>
-        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-      );
-  }, [url]); // 每当其依赖数组[url]改变时这个 Hook 就会创建一个 memoized 函数(即重新定义handleFetchStories函数),useEffect Hook 依赖于新的函数所以它会再次运行
+    try {
+      const result = await axios.get(url);
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
+  }, [url]);
+  // 每当其依赖数组[url]改变时这个 Hook 就会创建一个 memoized 函数(即重新定义handleFetchStories函数),useEffect Hook 依赖于新的函数所以它会再次运行
 
   React.useEffect(() => {
     handleFetchStories();
   }, [handleFetchStories]);
 
-  const handleSearch = (event) => {
+  const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
   };
   const handleSearchSubmit = () => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
   };
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -83,20 +82,11 @@ function App() {
 
   return (
     <div>
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        onInputChange={handleSearch}
-      >
-        Search
-      </InputWithLabel>
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       {stories.isError && <p>Something went wrong ...</p>}
 
@@ -111,6 +101,26 @@ function App() {
     </div>
   );
 }
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({
   id,
@@ -141,7 +151,7 @@ const Item = ({ item, onRemoveItem }) => (
     <span> {item.points}</span>
     <span>
       <button type="button" onClick={onRemoveItem.bind(null, item)}>
-        Dismiss
+        删除
       </button>
     </span>
   </div>

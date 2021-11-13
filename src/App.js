b7a3489993a50a2cd9073f45d74478f5b4ce646f
jjiahuang <15602215864@163.com>
2021-11-13 15:41:25 +0800

    添加Reducer，合并加载、报错、数据状态

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React from "react";
 
-// 自定义hook
-const useSemiPersistentState = (keyy, initialState) => {
-  const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState
-  );
-  React.useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value, key]);
-  return [value, setValue];
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          story => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
 };
 
 function App() {
-  const stories = [
+  const getAsyncStories = () =>
+    new Promise(resolve =>
+      setTimeout(
+        () => resolve({ data: { stories: initialStories } }),
+        2000
+      )
+    );
+  const initialStories = [
     {
       title: "React",
       url: "https://reactjs.org/",
@@ -31,18 +60,38 @@ function App() {
     },
   ];
   const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem("search") || "React"
+    localStorage.getItem("search") || ""
   );
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, { data: [], isLoading: false, isError: false });
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
   React.useEffect(() => {
     localStorage.setItem("search", searchTerm);
   }, [searchTerm]);
+  React.useEffect(() => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+    getAsyncStories().then(result => {
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.stories,
+      });
+      setIsLoading(false);
+    }).catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+  }, []);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-  const searchedStories = stories.filter(function (story) {
+  const searchedStories = stories.data.filter(function (story) {
     return story.title.toLowerCase().includes(searchTerm.toLowerCase());
   });
+  const handleRemoveStory = (item) => {
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+  };
 
   return (
     <div>
@@ -53,7 +102,17 @@ function App() {
       >
         Search
       </InputWithLabel>
-      <List list={searchedStories} />
+
+      {stories.isError && <p>Something went wrong ...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List
+          list={searchedStories}
+          onRemoveItem={handleRemoveStory}
+        />
+      )}
     </div>
   );
 }
@@ -72,10 +131,12 @@ const InputWithLabel = ({
   </>
 );
 
-const List = ({ list }) =>
-  list.map((item) => <Item key={item.objectID} item={item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
 
-const Item = ({ item }) => (
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -83,6 +144,11 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={onRemoveItem.bind(null, item)}>
+        Dismiss
+      </button>
+    </span>
   </div>
 );
 export default App;

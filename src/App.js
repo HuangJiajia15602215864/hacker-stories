040516b8a18a431c084ad6cc7d58250c9942b4fb
jjiahuang <15602215864@163.com>
2021-11-13 14:57:25 +0800

    提交react父子组件、props和state、hook、组件组合内容

--- src/App.js
+++ src/App.js
@@ -1,104 +1,88 @@
 import React from "react";
-const storiesReducer = (state, action) => {
-  if (action.type === 'SET_STORIES') {
-    return action.payload;
-  } else {
-    throw new Error();
-  }
-};
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-// 函数组件
-const App = () => {
-  const [stories, dispatchStories] = React.useReducer(
-    storiesReducer,
-    []
+
+// 自定义hook
+const useSemiPersistentState = (keyy, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
   );
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+  return [value, setValue];
 };
 
-const List = props =>
-  props.list.map(item => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-    </div>
-  ));
-
-const storiesReducer = (state, action) => {
-  if (action.type === 'SET_STORIES') {
-    return action.payload;
-  } else if (action.type === 'REMOVE_STORY') {
-    return state.filter(
-      story => action.payload.objectID !== story.objectID
-    );
-  } else {
-    throw new Error();
-  }
-};
-const App = () => {
-  const [stories, dispatchStories] = React.useReducer(
-    storiesReducer,
-    []
+function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem("search") || "React"
   );
-  const [searchTerm, setSearchTerm] = React.useState('React');
-  const handleSearch = event => {
+  React.useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
+
+  const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
   const searchedStories = stories.filter(function (story) {
-    return story.title.includes(searchTerm);
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
   });
-  React.useEffect(() => {
-    setIsLoading(true);
-    getAsyncStories()
-      .then(result => {
-        dispatchStories({
-          type: 'SET_STORIES',
-          payload: result.data.stories,
-        });
-        setIsLoading(false);
-      })
-      .catch(() => setIsError(true));
-  }, []);
-  const handleRemoveStory = item => {
-    dispatchStories({
-      type: 'REMOVE_STORY',
-      payload: item,
-    });
-  };
 
   return (
     <div>
-      <Search search={searchTerm} onSearch={handleSearch} />
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      >
+        Search
+      </InputWithLabel>
+      <List list={searchedStories} />
     </div>
   );
-};
+}
 
-const Search = ({ search, onSearch }) => (
+const InputWithLabel = ({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  children,
+}) => (
+  <>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
+    <input id={id} type={type} value={value} onChange={onInputChange} />
+  </>
+);
+
+const List = ({ list }) =>
+  list.map((item) => <Item key={item.objectID} item={item} />);
+
+const Item = ({ item }) => (
   <div>
-    <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
-    />
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </div>
 );
 export default App;
